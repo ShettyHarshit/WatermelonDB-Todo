5254f5a556aef0f5c97b8bf478069ebafe07cfad
Harshit Shetty <swaharsh@gmail.com>
2018-12-29 13:50:49 +0530

    Config [Android Setup for WatermelonDB]

--- android/app/src/main/java/com/wdb/MainApplication.java
+++ android/app/src/main/java/com/wdb/MainApplication.java
@@ -7,7 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import com.nozbe.watermelondb.WatermelonDBPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +23,14 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
+      );
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new WatermelonDBPackage() // ⬅️ Here!
       );
     }
 
