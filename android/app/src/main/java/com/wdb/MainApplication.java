1e6380c778d8b925ebd5da334976a8609fcbed2b
Harshit Shetty <swaharsh@gmail.com>
2018-12-29 14:30:08 +0530

    Fix [Build Errors]

--- android/app/src/main/java/com/wdb/MainApplication.java
+++ android/app/src/main/java/com/wdb/MainApplication.java
@@ -22,13 +22,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
         new WatermelonDBPackage() // ⬅️ Here!
       );
